5f3a8eebc99d50b6ccc3e0f5078b27b410227957
edenkonde7 <edenkonde@gmail.com>
2024-07-10 11:41:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='my-auto mx-auto'>
+        <div className=''>
+          <p className='italic'>Maestro in development</p>
+          <p className='italic text-slate-400'>This is the boilerplate made for high learning skills, creative website in responsive and processing new tcehnologies for development.</p>
+
+
+
+          <div className='flex justify-center'>
+            <a href="https://vitejs.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
